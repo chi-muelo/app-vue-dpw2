423be8180bd90cfe8f6dad6dc98af29d225d48ca
Noemi Vanesa Mendoza Limachi <noemimendoza-sif@itpm.edu.bo>
2026-06-26 09:52:09 -0400

    paso08

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-//import { createApp } from 'vue'
-//import './style.css'
-//import App from './App.vue'
-//import 'bootstrap/dist/css/bootstrap.min.css'
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-//createApp(App).mount('#app')
-// src/main.ts
+createApp(App).mount('#app')
+ src/main.ts
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
